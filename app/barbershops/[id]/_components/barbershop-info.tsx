944c1af7ec2294fbcb376060e561ea94380cfe71
Wesley Cicote <wcicote@gmail.com>
2024-02-15 20:37:52 -0300

    feat: add SideMenu component

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,55 +1,77 @@
-"use client"
+"use client";
 
+import SideMenu from "@/app/_components/side-menu";
 import { Button } from "@/app/_components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Barbershop } from "@prisma/client";
 import { ChevronLeftIcon, MenuIcon, MapPinIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 interface BarbershopInfoProps {
-  barbershop: Barbershop
+  barbershop: Barbershop;
 }
 
-const BarbershopInfo = ({barbershop}: BarbershopInfoProps) => {
-  const router = useRouter()
-  
-  const handleBackClick = () => {
-    router.replace("/")
-  }
+const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
+  const router = useRouter();
 
-  return ( 
+  const handleBackClick = () => {
+    router.replace("/");
+  };
+
+  return (
     <div>
       <div className="h-[250px] w-full relative">
-        <Button onClick={handleBackClick} size="icon" variant="outline" className="z-50 absolute top-4 left-4" >
+        <Button
+          onClick={handleBackClick}
+          size="icon"
+          variant="outline"
+          className="z-50 absolute top-4 left-4"
+        >
           <ChevronLeftIcon />
         </Button>
 
-        <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4" >
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger>
+            <Button
+              size="icon"
+              variant="outline"
+              className="z-50 absolute top-4 right-4"
+            >
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
 
-        <Image src={barbershop.imageUrl} fill alt={barbershop.name} style={{
-          objectFit: "cover"
-        }}
-        className="opacity-75"
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
+
+        <Image
+          src={barbershop.imageUrl}
+          fill
+          alt={barbershop.name}
+          style={{
+            objectFit: "cover",
+          }}
+          className="opacity-75"
         />
       </div>
 
       <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
         <h1 className="text-xl font-bold ">{barbershop.name}</h1>
         <div className="flex item-center gap-1 mt-2">
-          <MapPinIcon className="text-[#8162FF]" size={18}/>
+          <MapPinIcon className="text-[#8162FF]" size={18} />
           <p className="text-sm">{barbershop.address}</p>
         </div>
 
         <div className="flex item-center gap-1 mt-2">
-          <StarIcon className="text-[#8162FF]" size={18}/>
+          <StarIcon className="text-[#8162FF]" size={18} />
           <p className="text-sm">5,0 (899 avaliações)</p>
         </div>
-        
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default BarbershopInfo;
