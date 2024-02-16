19cdb873450c1427f7e77ab59715df98dbed99a1
Wesley Cicote <wcicote@gmail.com>
2024-02-16 17:07:17 -0300

    feat: ensure a service can be booked by the user

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -1,23 +1,19 @@
 "use client";
 
-import { Button } from "@/app/_components/ui/button";
-import { Calendar } from "@/app/_components/ui/calendar";
-import { Card, CardContent } from "@/app/_components/ui/card";
-import {
-  Sheet,
-  SheetContent,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/app/_components/ui/sheet";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Barbershop, Service } from "@prisma/client";
 import { ptBR } from "date-fns/locale";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useMemo, useState } from "react";
 import { generateDayTimeList } from "../_helpers/hours";
-import { format } from "date-fns";
+import { format, setHours, setMinutes } from "date-fns";
+import { saveBooking } from "../_actions/save-booking";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Button } from "@/app/_components/ui/button";
+import { Calendar } from "@/app/_components/ui/calendar";
+import { Loader2 } from "lucide-react";
+
 
 interface ServiveItemProps {
   barbershop: Barbershop
@@ -26,26 +22,50 @@ interface ServiveItemProps {
 }
 
 const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiveItemProps) => {
+  const { data } = useSession();
+
   const [date, setDate] = useState<Date | undefined>(undefined);
-  const [hour, setHour] = useState<string | undefined>()
+  const [hour, setHour] = useState<string | undefined>();
+  const [submitIsLoading, setSubmitIsLoading] = useState(false);
 
   const handleDateClick = (date: Date | undefined) => {
-    setDate(date)
-    setHour(undefined)
-  } 
-
+    setDate(date);
+    setHour(undefined);
+  };
   const handleHourClick = (time: string) => {
-    setHour(time)
-  }
-
+    setHour(time);
+  };
   const handleBookingClick = () => {
     if (!isAuthenticated) {
       return signIn("google");
     }
-
-    //TODO: abrir modal de agendamento
   };
 
+  const handleBookingSubmit = async () => {
+    setSubmitIsLoading(true);
+
+    try {
+      if (!hour || !date || !data?.user) {
+        return;
+      }
+
+      const dateHour = Number(hour.split(":")[0]);
+      const dateMinutes = Number(hour.split(":")[1]);
+
+      const newDate = setMinutes(setHours(date, dateHour), dateMinutes);
+
+      await saveBooking({
+        serviceId: service.id,
+        barbershopId: barbershop.id,
+        date: newDate,
+        userId: (data.user as any).id,
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitIsLoading(false);
+    }
+  }
   const timeList = useMemo(() => {
     return date ? generateDayTimeList(date) : [];
   }, [date]);
@@ -178,7 +198,10 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiveItemProps)
                   </div>
 
                   <SheetFooter className="px-5">
-                    <Button disabled={!hour || !date}>Confirmar reserva</Button>
+                    <Button onClick={handleBookingSubmit} disabled={!hour || !date || submitIsLoading}>
+                      {submitIsLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                      Confirmar reserva
+                    </Button>
                   </SheetFooter>
                 </SheetContent>
               </Sheet>
@@ -190,4 +213,4 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiveItemProps)
   );
 };
 
-export default ServiceItem;
+export default ServiceItem
